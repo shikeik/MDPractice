1c775c35bde03d6a893f19a93cb6a1465c6d9fe5
shikeik <30600841+shikeik@users.noreply.github.com>
2023-09-24 01:01:28 +0800

    测试提交

--- 自动提交.bat
+++ 自动提交.bat
@@ -1,3 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem 添加所有修改
 git add .
-git commit -m "auto checkin in bat."
+
+rem 提示用户输入 commit 信息
+set /p commit_message=请输入 commit 信息: 
+
+rem 提交更改
+git commit -m "!commit_message!"
+
+rem 推送到远程仓库
 git push origin
+
+endlocal
